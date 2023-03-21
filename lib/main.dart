54cef01949f74677154f3470d4b4413a5c4826e5
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-03-21 11:41:48 -0300

    Row + TextButtons created

--- lib/main.dart
+++ lib/main.dart
@@ -19,29 +19,62 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void increment() {
+    print('+');
+  }
+
+  void decrement() {
+    print('-');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         // ignore: unnecessary_const
-        children: const [
-          Text(
-            'Pode entrar!',
+        children: [
+          const Text(
+            'Há vagas!',
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.w700,
               color: Colors.white,
             ),
           ),
-          Text(
+          const Text(
             '0',
             style: TextStyle(
               fontSize: 100,
               fontWeight: FontWeight.w500,
               color: Colors.white,
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: increment,
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: decrement,
+                child: const Text(
+                  'Sair',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
