68d0e315a1cd70da5c611d040a497428fc6b7ca8
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-11 19:34:33 -0300

    TextField

--- lib/app02/tasklist_app.dart
+++ lib/app02/tasklist_app.dart
@@ -5,9 +5,27 @@ class TaskListApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Container(
-        color: Colors.blue,
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-Mail',
+              hintText: 'seuemail@provedor.com',
+              border: OutlineInputBorder(),
+              labelStyle: TextStyle(
+                fontSize: 25,
+                color: Colors.blue,
+              ),
+            ),
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+          ),
+        ),
       ),
     );
   }
