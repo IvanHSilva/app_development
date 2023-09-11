baff11a5a1e9c72b01720e7d72c9cac2b11f3a7f
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-11 18:49:16 -0300

    TaskList app structure created

---
+++ lib/app02/tasklist_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TaskListApp extends StatelessWidget {
+  const TaskListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      child: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'app01/park_app.dart';
+//import 'app01/park_app.dart';
+import 'app02/tasklist_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ParkApp(),
+      //home: ParkApp(),
+      home: TaskListApp(),
+      //home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
