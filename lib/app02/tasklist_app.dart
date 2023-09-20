10f653308e73f9bdf6e155c5fd7c0f06a84efe2d
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-09-20 15:00:44 -0300

    TextEditingController

--- lib/app02/tasklist_app.dart
+++ lib/app02/tasklist_app.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
 
 class TaskListApp extends StatelessWidget {
-  const TaskListApp({super.key});
+  TaskListApp({super.key});
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'E-Mail',
-              hintText: 'seuemail@provedor.com',
-              border: OutlineInputBorder(),
-              labelStyle: TextStyle(
-                fontSize: 25,
-                color: Colors.blue,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-Mail',
+                  hintText: 'email@provedor.com',
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                  ),
+                ),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
+                ),
               ),
-            ),
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.purple,
-            ),
+              ElevatedButton(
+                onPressed: enter,
+                child: const Text('Entrar'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void enter() {
+    String message = emailController.text;
+    print(message);
+    emailController.clear();
   }
 }
