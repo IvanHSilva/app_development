fd2f1121efae39951af657efe7c84aa28b9c539f
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-10-05 20:27:08 -0300

    Slidable widget

--- lib/app02/widgets/tasklistitem.dart
+++ lib/app02/widgets/tasklistitem.dart
@@ -1,5 +1,6 @@
 import 'package:app_development/app02/models/tasklist.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 class TaskListItem extends StatelessWidget {
@@ -9,30 +10,41 @@ class TaskListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      margin: const EdgeInsets.symmetric(vertical: 2.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[200],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy HH:mm').format(task.date),
-            style: const TextStyle(
-              fontSize: 12,
+    return Slidable(
+      actionPane: const SlidableDrawerActionPane(),
+      secondaryActions: [
+        IconSlideAction(
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () {},
+        ),
+      ],
+      actionExtentRatio: 0.25,
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        margin: const EdgeInsets.symmetric(vertical: 2.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.grey[200],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy HH:mm').format(task.date),
+              style: const TextStyle(
+                fontSize: 12,
+              ),
             ),
-          ),
-          Text(
-            task.title,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w700,
+            Text(
+              task.title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
