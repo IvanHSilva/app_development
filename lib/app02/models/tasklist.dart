d539c3c935c0f0f0b3c6f5c81a581c03841de4ce
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-10-29 11:05:01 -0300

    TaskList read

--- lib/app02/models/tasklist.dart
+++ lib/app02/models/tasklist.dart
@@ -1,6 +1,10 @@
 class TaskList {
   TaskList({required this.title, required this.date});
 
+  TaskList.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        date = DateTime.parse(json['date']);
+
   String title;
   DateTime date;
 
--- lib/app02/repositories/tasklist_repository.dart
+++ lib/app02/repositories/tasklist_repository.dart
@@ -3,16 +3,24 @@ import 'dart:convert';
 import 'package:app_development/app02/models/tasklist.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class TaskListRepository {
-  TaskListRepository() {
-    SharedPreferences.getInstance().then((value) => sharePref = value);
-  }
+const taskListKey = 'tasklist';
 
+class TaskListRepository {
+  //
   late SharedPreferences sharePref;
 
+  Future<List<TaskList>> loadTaskList() async {
+    //
+    sharePref = await SharedPreferences.getInstance();
+    final String jsonText = sharePref.getString(taskListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonText) as List;
+    return jsonDecoded.map((e) => TaskList.fromJson(e)).toList();
+  }
+
   void saveTaskList(List<TaskList> tasks) {
+    //
     final String jsonText = json.encode(tasks);
-    sharePref.setString('tasklist', jsonText);
+    sharePref.setString(taskListKey, jsonText);
     //print(jsonText);
   }
 }
