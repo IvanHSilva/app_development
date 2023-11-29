af8d1969453ab303ad98a0b34d60eb4c1300a1cf
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-11-29 15:17:29 -0300

    Controllers and calculations

--- lib/app03/imc_calc_app.dart
+++ lib/app03/imc_calc_app.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
 
-class IMCCalcApp extends StatelessWidget {
+class IMCCalcApp extends StatefulWidget {
   const IMCCalcApp({super.key});
+
+  @override
+  State<IMCCalcApp> createState() => _IMCCalcAppState();
+}
+
+class _IMCCalcAppState extends State<IMCCalcApp> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _info = 'Informe seu peso e altura';
+
+  void _resetField() {
+    weightController.text = '';
+    heightController.text = '';
+  }
+
+  void imcCalculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      _info = '';
+      if (imc >= 40.0) {
+        _info = 'Obesidade Grau 3';
+      }
+      if (imc >= 34.9) {
+        _info = 'Obesidade Grau 2';
+      }
+      if (imc >= 29.9) {
+        _info = 'Obesidade Grau 1';
+      }
+      if (imc >= 24.9) {
+        _info = 'Levemente Acima do Peso';
+      }
+      if (imc >= 18.6) {
+        _info = 'Peso Ideal';
+      }
+      if (imc < 18.6) {
+        _info = 'Abaixo do Peso';
+      }
+      if (_info.isNotEmpty) {
+        _info += ' (${imc.toStringAsPrecision(3)})';
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +60,7 @@ class IMCCalcApp extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _resetField,
             icon: const Icon(Icons.refresh),
           ),
         ],
@@ -32,14 +76,15 @@ class IMCCalcApp extends StatelessWidget {
               size: 120,
               color: Colors.green,
             ),
-            const TextField(
+            TextField(
+              controller: weightController,
               keyboardType: TextInputType.number,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 25,
                 color: Colors.green,
               ),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Peso (Kg)',
                 labelStyle: TextStyle(
                   fontSize: 30,
@@ -47,14 +92,15 @@ class IMCCalcApp extends StatelessWidget {
                 ),
               ),
             ),
-            const TextField(
+            TextField(
+              controller: heightController,
               keyboardType: TextInputType.number,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 25,
                 color: Colors.green,
               ),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Altura (cm)',
                 labelStyle: TextStyle(
                   fontSize: 30,
@@ -67,7 +113,7 @@ class IMCCalcApp extends StatelessWidget {
               child: SizedBox(
                 height: 50,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: imcCalculate,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
                   ),
@@ -81,10 +127,10 @@ class IMCCalcApp extends StatelessWidget {
                 ),
               ),
             ),
-            const Text(
-              'Info',
+            Text(
+              _info,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 25,
                 color: Colors.green,
               ),
